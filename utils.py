529f06c5f28104f7a52baeeb2c35d30ed30c2f81
Jerie Wang <mail@jerie.wang>
2020-10-12 00:31:42 -0700

    Backward compatible with python3.7

--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ import re
 allowed_chars = re.compile(r'^[A-Za-z0-9_-]+$')
 # yes extensions will be provided
 
-def validate_filename(s: str, /):
+def validate_filename(s: str):
     return re.fullmatch(allowed_chars, s) is not None
