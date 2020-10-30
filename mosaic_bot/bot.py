12f914086834220b9cbfaf0151a00b174d2f31cd
Mia Celeste <mia@miaceleste.dev>
2020-10-30 02:04:13 -0700

    Added MessageManager class

--- mosaic_bot/bot.py
+++ mosaic_bot/bot.py
@@ -4,18 +4,21 @@ import random
 import re
 import sys
 from asyncio import sleep
+import asyncio
 from dataclasses import dataclass
-from typing import List, Union
-
+from typing import List, Union, Dict
+import sqlite3
 import aiohttp
 import discord
 from PIL import Image
 from discord.ext import commands
+import re
 
 from mosaic_bot.credentials import MOSAIC_BOT_TOKEN
 from mosaic_bot.emojis import get_emoji_by_rgb
-from mosaic_bot.image import gen_emoji_sequence
+from mosaic_bot.image import gen_emoji_sequence, gen_gradient
 from mosaic_bot.utils import validate_filename
+import pathlib
 
 # ---------------- logging ----------------
 
@@ -53,6 +56,88 @@ HELP_TEXT = """
 ```
 For a more detailed description, please visit https://mosaic.by.jerie.wang/references.
 """
+
+BASE_PATH = pathlib.Path(__file__).resolve().parent.parent
+ICON = (f := open(BASE_PATH / 'icon.png', 'br')).read()
+f.close()
+
+
+def get_db():
+    db = sqlite3.connect(str((BASE_PATH / 'db.sqlite3')))
+    return db
+
+
+async def get_webhook(channel_id: int) -> discord.Webhook:
+    channel: discord.TextChannel = await bot.fetch_channel(channel_id)
+    webhooks: List[discord.Webhook] = await channel.webhooks()
+    for wh in webhooks:
+        if wh.user == bot.user:
+            webhook = wh
+            break
+    else:
+        webhook = await channel.create_webhook(name=bot.user.name, avatar=ICON,
+                                               reason='Created webhook for sending emojis')
+    return webhook
+
+
+class RequestInterrupted(Exception): pass
+
+
+class EmojiSequenceTooLong(Exception): pass
+
+
+class MessageManager:
+    channel_locks = set()
+    active_managers = {}
+    
+    def __init__(self, ctx: commands.Context):
+        self.channel = ctx.channel.id
+        self.destination = ctx
+        self.message_ids: List[int] = []
+        self.requesting_message: int = ctx.message.id
+        self.requester: discord.User = ctx.message.author.id
+        self.active_managers[self.requesting_message] = self
+        self.is_interrupted = False
+    
+    async def __aenter__(self):
+        while self.channel in self.channel_locks:
+            await sleep(1.5)
+        self.channel_locks.add(self.channel)
+        return self
+    
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self.channel_locks.remove(self.channel)
+        # TODO: write sent messages to db
+        del self.active_managers[self.requesting_message]
+        if exc_type==RequestInterrupted:
+            return True
+    
+    async def send(self, *args, trigger_typing=False, use_webhook=False, **kwargs):
+        if self.is_interrupted:
+            raise RequestInterrupted
+            # this will be caught in the __aexit__ function and silenced
+            # raise this exception to interrupt the normal code flow
+            # to save some CPU power.
+        if use_webhook:
+            wh = await get_webhook(self.channel)
+            msg = await wh.send(*args, wait=True)
+        else:
+            msg = await self.destination.send(*args, **kwargs)
+        if trigger_typing:
+            await self.destination.trigger_typing()
+        self.message_ids.append(msg.id)
+        return msg
+    
+    def interrupt(self):
+        self.is_interrupted = True
+        asyncio.ensure_future(delete_messages(self.channel,self.message_ids))
+    
+    @classmethod
+    def message_deleted(cls, m_id: int):
+        if m_id in cls.active_managers:
+            cls.active_managers[m_id].interrupt()
+        
+        # TODO: check db for sent messages and delete all
 
 
 class lock_channel:
@@ -104,9 +189,6 @@ async def delete_messages(cid: int, msgs: List[int], bulk=True):
                         },
                 )
                 await sleep(0.5)
-
-
-class EmojiSequenceTooLong(Exception): pass
 
 
 def split_minimal(seq: str, strip_end=True):
@@ -274,18 +356,44 @@ async def minecraft(ctx, *, raw_args=''):
     await show(ctx, raw_or_parsed_args=opts)
 
 
+@bot.command()
+async def gradient(ctx: commands.Context, *, raw_args=''):
+    async with MessageManager(ctx) as manager:
+        opts = parse_opt(raw_args)
+        c = re.search(r'\b(?P<c>[rgb]|red|green|blue)=(?P<v>\d{1,2})\b', raw_args)
+        if c is None:
+            await manager.send("You need to specify a base color because I can't send a 3D image")
+            return
+        if not 0 <= int(c.group('v')) <= 15:
+            await manager.send("You see, I only know 16 colors so I can't do that")
+            return
+        color = {c.group('c')[0]: int(c.group('v'))}
+        img = gen_gradient(**color)
+        emojis = gen_emoji_sequence(img, opts.large, opts.no_space)
+        if opts.large:
+            seq = emojis.splitlines()
+            for msg in seq:
+                await manager.send(msg, use_webhook=True)
+                await sleep(1)
+        else:
+            for i in range(4):
+                await manager.send('\n'.join(emojis.splitlines()[i * 4:i * 4 + 4]), use_webhook=True)
+
+
 @bot.event
 async def on_raw_message_delete(e: discord.RawMessageDeleteEvent):
     if e.channel_id in locks:
         interrupted.add(e.message_id)
     elif e.message_id in sent_msgs:
         await delete_messages(e.channel_id, sent_msgs[e.message_id])
+    MessageManager.message_deleted(e.message_id)
 
 
 @bot.event
 async def on_ready():
     print('Logged in as ' + bot.user.name + '#' + bot.user.discriminator)
     await bot.change_presence(activity=discord.Activity(name='|show help', type=discord.ActivityType.playing))
+    await get_webhook(764939692478431232)
 
 
 if __name__ == '__main__':
