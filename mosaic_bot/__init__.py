3bcb80de3534ea21124319db105f03e8a7f8fb5e
Mia Celeste <mia@miaceleste.dev>
2021-01-22 00:13:07 -0800

    Added dynamic build id calculation

--- mosaic_bot/__init__.py
+++ mosaic_bot/__init__.py
@@ -1,12 +1,27 @@
 import pathlib
 import PIL.Image
+from hashlib import sha1
+import os
 
 BASE_PATH = pathlib.Path(__file__).resolve().parent.parent
 IMAGE_DIR = './new_images/'
 __version__ = '0.1'
 # versions will be numbered after the golden ratio, like how LaTeX does it
 
+hasher = sha1()
+for root, dir, files in os.walk(BASE_PATH / 'mosaic_bot'):
+    for fp in files:
+        if fp.endswith('.py'):
+            with open(BASE_PATH / root / fp, 'rb') as f:
+                hasher.update(f.read())
+
+__build__ = hasher.hexdigest()
 
 PIL.Image.MAX_IMAGE_PIXELS = 4000 ** 2
 
-__all__ = ['BASE_PATH', 'IMAGE_DIR']
+__all__ = [
+    '__version__',
+    '__build__',
+    'BASE_PATH',
+    'IMAGE_DIR',
+]
